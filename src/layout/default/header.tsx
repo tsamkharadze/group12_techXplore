288ad4b9bda53960eae34379022964a29e4fc52c
tsamkharadze <samkaradze@gmail.com>
2025-02-22 00:55:34 +0400

    add vercel.json

--- src/layout/default/header.tsx
+++ src/layout/default/header.tsx
@@ -58,7 +58,7 @@ export const Header = () => {
             <DialogContent className="sm:hidden">
               <nav className="flex flex-col space-y-4">
                 <NavLink
-                  to="/home"
+                  to="/"
                   className="text-sm font-medium text-gray-500 hover:text-gray-700"
                 >
                   Home
